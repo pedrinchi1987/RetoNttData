4976f25b239c5e449876dcfe4dffffc9a6252704
PEDRO MONTENEGRO <pedrito1348@gmail.com>
2026-02-13 16:14:35 -0500

    ajuste_destroy_iac_9

--- infra/terraform/lambda_authorizer.tf
+++ infra/terraform/lambda_authorizer.tf
@@ -1,10 +1,16 @@
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda.zip"
+}
+
 resource "aws_lambda_function" "jwt_authorizer" {
-  filename         = "${path.module}/lambda/lambda_authorizer.zip"
+  filename         = data.archive_file.lambda_zip.output_path
   function_name    = "${var.service_name}-jwt-authorizer"
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = "index.handler"
   runtime          = "nodejs20.x"
-  source_code_hash = filebase64sha256("${path.module}/lambda/lambda_authorizer.zip")
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   timeout          = 10
   environment {
     variables = {
