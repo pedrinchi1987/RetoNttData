d79e6c27fa4e1ddc9c69ed82f5e2c78cf8cbe268
PEDRO MONTENEGRO <pedrito1348@gmail.com>
2026-02-13 23:18:10 -0500

    ajuste_ecs_3

--- infra/terraform/general/outputs.tf
+++ infra/terraform/general/outputs.tf
@@ -1,15 +1,3 @@
-output "ecr_repository_url" {
-  value = aws_ecr_repository.devops_repo.repository_url
-}
-
-output "alb_dns_name" {
-  value = aws_lb.devops_alb.dns_name
-}
-
 output "api_gateway_id" {
   value = aws_api_gateway_rest_api.devops_api.id
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.devops_cluster.name
 }
