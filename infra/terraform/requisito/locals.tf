19d57f282398e38c92c725e2da6e4fc844ac8d95
PEDRO MONTENEGRO <pedrito1348@gmail.com>
2026-02-16 22:19:46 -0500

    pruebas_obtener_datos_task_4

--- infra/terraform/requisito/locals.tf
+++ infra/terraform/requisito/locals.tf
@@ -1,3 +1,9 @@
 locals {
-  image_tag = var.image_tag
+  #image_tag = var.image_tag
+  image_tag = split(":", jsondecode(data.aws_ecs_task_definition.devops_task.container_definitions)[0].image)[1]
+}
+
+
+output "image_tag" {
+  value       = local.image_tag
 }
