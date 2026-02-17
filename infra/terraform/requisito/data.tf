86dc81ea1c6efaca5e6bb7de76756fc6910a179f
PEDRO MONTENEGRO <pedrito1348@gmail.com>
2026-02-16 22:26:02 -0500

    pruebas_obtener_datos_task_5

--- infra/terraform/requisito/data.tf
+++ infra/terraform/requisito/data.tf
@@ -1,13 +1,13 @@
 data "aws_ecs_task_definition" "devops_task" {
-  #count    = var.image_tag=="null"?1:0
+  count    = var.image_tag=="null"?1:0
   task_definition = "${var.service_name}-${var.env}"
 }
 
 output "devops_task" {
-  value = data.aws_ecs_task_definition.devops_task.container_definitions
+  value = data.aws_ecs_task_definition.devops_task[0].container_definitions
 }
 
 output "devops_task2" {
-  value = try(jsondecode(data.aws_ecs_task_definition.devops_task.container_definitions)[0].image, "ERROR")
+  value = try(jsondecode(data.aws_ecs_task_definition.devops_task[0].container_definitions)[0].image, "ERROR")
 }
 
--- infra/terraform/requisito/locals.tf
+++ infra/terraform/requisito/locals.tf
@@ -1,6 +1,6 @@
 locals {
   #image_tag = var.image_tag
-  image_tag = split(":", jsondecode(data.aws_ecs_task_definition.devops_task.container_definitions)[0].image)[1]
+  image_tag = split(":", jsondecode(data.aws_ecs_task_definition.devops_task[0].container_definitions)[0].image)[1]
 }
 
 
