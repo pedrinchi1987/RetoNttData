b42c12cd5039756f9b4b3509f42bde200ab3b4e7
PEDRO MONTENEGRO <pedrito1348@gmail.com>
2026-02-16 23:21:55 -0500

    arreglar_destroy_prueba_2

--- infra/terraform/requisito/data.tf
+++ infra/terraform/requisito/data.tf
@@ -1,5 +1,5 @@
 data "aws_ecs_task_definition" "devops_task" {
-  count           = var.image_tag == "null" ? 1 : 0
+  count           = var.image_tag == "cero" ? 1 : 0
   task_definition = "${var.service_name}-${var.env}"
 }
 
--- infra/terraform/requisito/locals.tf
+++ infra/terraform/requisito/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  image_tag = var.image_tag == "null" ? try(split(":", jsondecode(data.aws_ecs_task_definition.devops_task[0].container_definitions)[0].image)[1], "") : var.image_tag
+  image_tag = var.image_tag == "cero" ? try(split(":", jsondecode(data.aws_ecs_task_definition.devops_task[0].container_definitions)[0].image)[1], "") : var.image_tag
 }
 
 output "image_tag" {
